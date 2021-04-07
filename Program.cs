e08732853fbcec08c5f062b4f8dc09c1cf2810c7
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-04-07 15:51:02 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Encapsulamiento_Persona
+{
+    class Persona{
+        private string nombre;
+        private DateTime fecha_Nacimiento;
+        private int edad;
+        public Persona(string nombre){
+            this.nombre = nombre;
+            this.fecha_Nacimiento = new DateTime (1999,2,23);
+        }
+        public void Registrar_Persona(){
+            Console.WriteLine("\nEl nombre de la Persona es {0}, nacio el {1}, y tiene {2} años\n", nombre, fecha_Nacimiento, edad);
+        }
+        public void CalcularEdad(){
+            DateTime Hoy = DateTime.Now;
+            var today = DateTime.Today;
+            edad = today.Year - fecha_Nacimiento.Year;
+            if (fecha_Nacimiento.Date > today.AddYears(-edad)) edad--;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona("Clara");
+            p.CalcularEdad();
+            p.Registrar_Persona();
+
+        }
+    }
+}
